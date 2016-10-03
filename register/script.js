a55382972ed477b41a912269af6b5a8ef18aceac
Linh Ho <homautuanlinh@gmail.com>
2016-10-03 15:05:48 +0700

    Add animation

--- register/script.js
+++ register/script.js
@@ -1,3 +1,4 @@
+
 var checked = false;
 
 function passwordValidation(pass) {
@@ -5,25 +6,30 @@ function passwordValidation(pass) {
     //var password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
     var password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z0-9\d$@$!%*?&]{4,}/;
     notify('passDiv',password.test(test));
+    return(password.test(test));
 }
 function nameValidation(name) {
     test = name;
     var name = /^[a-zA-Z0-9]{6,}/;
     notify('nameDiv',name.test(test));
+    return(name.test(test));
 }
 function emailValidation(email) {
     test = email;
     var email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     notify('emailDiv',email.test(test));
+    return(email.test(test));
 }
 function rePasswordValidation(rePass, pass) {
     test = rePass;
     pass = pass;
     if(test === pass) {
         notify('repassDiv',true);
+        return(true);
     }
     else {
         notify('repassDiv',false);
+        return(false);
     }
     
 }
@@ -38,6 +44,7 @@ function validation() {
     emailValidation(email);
     rePasswordValidation(rePass, pass);
     checked = true;
+    notifyBoxRegister(nameValidation(name), emailValidation(email), passwordValidation(pass), rePasswordValidation(rePass, pass));
 }
 
 function notify(id, check) {
@@ -57,4 +64,12 @@ function notify(id, check) {
         para.appendChild(node);
     }
     parent.appendChild(para);
+}
+
+function notifyBoxRegister(user, mail, pass, repass) {
+    if(user==false || mail==false || pass==false || repass==false){
+        var box = document.getElementsByClassName('registerDiv')[0];
+        box.classList.add('notify');
+        setTimeout(function(){ box.classList.remove('notify'); },1000);
+    }
 }
