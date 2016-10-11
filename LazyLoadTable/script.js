3d28562bd554044ed06c38893ef3a9e2ba946932
Linh Ho <homautuanlinh@gmail.com>
2016-10-11 15:11:41 +0700

    Update Lazyload table

--- LazyLoadTable/script.js
+++ LazyLoadTable/script.js
@@ -1,2 +1,8 @@
-userTable = document.getElementsById("userTable");
-userTable.appendChild()
+var userTable = document.getElementsById("userTable");
+
+var trParam = document.createElement('tr');
+var tdParam = document.createElement('td');
+var node    = document.createTextNode('hello');
+tdParam.appendChild(node);
+trParam.appendChild(tdParam);
+userTable.appendChild(trParam);
