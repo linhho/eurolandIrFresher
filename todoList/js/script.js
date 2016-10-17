8dfd5d2dda7887571709842978cc602fb396304e
Linh Ho <homautuanlinh@gmail.com>
2016-10-17 13:14:33 +0700

    done todo list

--- todoList/js/script.js
+++ todoList/js/script.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
-    $('#addListContent').hide();
     $('#addList').click(function(){
-        $(this).hide();
-        $('#addListContent').show();
+        $(this).hide('fast');
+        $('#addListContent').show('fast');
     });
     $('#taskTextField').keypress(function (e) {
         var key = e.which;
         if(key == 13)  // the enter key code
         {
             taskName = $(this).val();
-            $('ul#todolist').append('<li>'+taskName+'</li>');
-            $('#addListContent').hide();
-            $('#addList').show();
+            $('ul#todolist').append('<li class="task">'+taskName+'<a class="removeTask" href="#"><i class="material-icons right red-text">clear</i></a></li>').show('slow');
+            $('#addListContent').hide('fast');
+            $(this).val('');
+            $('#addList').show('fast');
             return false;  
         }
     });
+
+    $('ul#todolist').on('click', 'li a.removeTask i', function(e) {
+        $(this).parent().parent().fadeOut("slow", function(){
+            $(this).remove();
+        });
+    });
+    $( function() {
+        $( "#todolist" ).sortable();
+        $( "#todolist" ).disableSelection();
+      } );
 });
