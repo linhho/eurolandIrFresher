219549b3a84f38db460c192548a8fcf3ce0c870e
Linh Ho <homautuanlinh@gmail.com>
2016-10-19 09:49:35 +0700

    Create final project

---
+++ Final project/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.parallax').parallax(); //call parallax
+    var scrollspyOptions =
+    {scrollOffset: 0}
+    $('.scrollspy').scrollSpy(scrollspyOptions); //call scrollspy
+});
