74a56d6139b1481fa979ad32277e3f70e4a9dc35
Linh Ho <homautuanlinh@gmail.com>
2016-10-21 14:50:20 +0700

    Question list: drag question and answer

---
+++ ListQuestion/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('select').material_select();
+    $( function() {
+        $( "#Question_list" ).sortable();
+        $( "#Question_list" ).disableSelection();
+    } );
+    $( function() {
+        $( ".an_content" ).sortable();
+        $( ".an_content" ).disableSelection();
+    } );
+});
